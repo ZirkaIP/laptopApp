29c147383be95e6f296fc9d1b3df9fe503ef428a
Ilia <i.pulman.official@gmail.com>
2019-08-04 14:50:22 +0300

    Initial commit

---
+++ OnlineStore/Data/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Data.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public string Desk { get; set; }
+        public List<Laptop> Laptops { get; set; }
+
+    }
+}
