858b31f4825930bc63e5bf99059d28825ea1b8bc
Dzianis Dziarkach <ddzia@outlook.com>
2020-06-19 22:47:10 +0300

    Revert "Revert "Revert "clear all data"""
    This reverts commit c3e7e7e94d9f9e95480e687dde97fbdeb29fa1d0.

---
+++ OnlineStore/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using OnlineStore.Data.Interfaces;
+using OnlineStore.Data.Models;
+using OnlineStore.ViewModels;
+
+namespace OnlineStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IAllLaptops _laptopRep;
+        
+        public HomeController(IAllLaptops laptopRep)
+        {
+            _laptopRep = laptopRep;
+        }
+
+        public ViewResult Index()
+        {
+            var homeLaptops = new HomeViewModel
+            {
+                FavLaptops = _laptopRep.GetFavLaptops
+            };
+            return View(homeLaptops);
+        }
+    }
+}
---
+++ OnlineStore/Migrations/20190728083218_Initial.cs
@@ -0,0 +1,65 @@
+﻿ using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineStore.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CategoryName = table.Column<string>(nullable: true),
+                    Desk = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Laptop",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    ShortDesc = table.Column<string>(nullable: true),
+                    LongDesc = table.Column<string>(nullable: true),
+                    IsFavorite = table.Column<bool>(nullable: false),
+                    Price = table.Column<int>(nullable: false),
+                    Img = table.Column<string>(nullable: true),
+                    IsAvailable = table.Column<bool>(nullable: false),
+                    CategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Laptop", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Laptop_Category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Laptop_CategoryId",
+                table: "Laptop",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Laptop");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+        }
+    }
+}
